b8200771d3dc6d09a563d7eac23cfe283ec1eb26
mubashiriftkhar <102500757+mubashiriftkhar@users.noreply.github.com>
2025-11-26 19:30:24 +0500

    Refactor taskPlannerNode for structured output and JSON
    Updated task planner to use structured output and save results to a JSON file.

--- Autonomous Market Research Agent/planneragent.py
+++ Autonomous Market Research Agent/planneragent.py
@@ -1,22 +1,19 @@
-# from langgraph.graph.message import add_messages
-import ast
 from state import State
-from client import client
-
+from client import llm
+from pydantic import BaseModel
+from typing import List
+import json
+class TaskList(BaseModel):
+    tasks:List[str]
 
 def taskPlannerNode(state:State)-> dict:
-    userQuery = state['messages'][-1]['content']
-    completion = client.chat.completions.create(
-    extra_body={},
-    model="mistralai/mistral-7b-instruct:free",
-    messages=[
-       {
-  "role": "system",
-  "content": (
-   """
+    print("Planner Agent Started")
+    userQuery = state["query"]
+    prompt= f"""
+    query:{userQuery}
 You are an expert Autonomous Market Research Planner Agent.
 Your role is to deeply analyze the given query and construct a well-structured, diverse research plan.
-We have three agents to work on user Query Finance Agent, Social Agent, Web Agent You have to assign task to these three agent that what these should do.
+We have to give these queries to Web Search agent to work on.
 Think step-by-step:
 1. First, carefully read the query and understand the user's real research need.
 2. Look for time interval and country where user want to search and which time interval user wants
@@ -25,41 +22,36 @@ Think step-by-step:
    - intent (choose exactly one from: trend_analysis, competitor_comparison, pricing, sentiment_analysis)
    - brands (list of strings)
    - products (list of strings)
-   - dates (tuple of start year and end year in "year-year" format)
+   - dates (tuple of start year and end year in "year-year" format) If Date is not in Query then add relevent date by yourself but if available then use given date.
    - locations (list of strings)
    - keywords (important words such as product names and country/place names)
-5. Based on these extracted details, decide where to get the information like social media platform or other and wirte name of that platform and Write it in a Python dictionary, write atleast 5 platform
+5. Generate Atleast 5 to 7 Sub Queries.
 
 
 Finally:
-- Respond ONLY with a valid Python dictionary.
+
+- Respond ONLY with a valid Python List.
 - Do not include your reasoning or intermediate steps in the final output.
-- The dict should be directly usable as Python code.
+- The List should be directly usable as Python code.
 
 """
-  )
-},
-{
-  "role": "user",
-  "content": f"Query: {userQuery}"
-}
-    ]
-    )
-    reply = completion.choices[0].message.content
+    structuredLLm=llm.with_structured_output(schema=TaskList)
+    response=structuredLLm.invoke(prompt)
+
     
-    try:
-        subTasks = ast.literal_eval(reply)
-    except Exception:
-        subTasks = [reply]
-    state['tasks']=subTasks
+    state["tasks"] = response.tasks
+    with open("hydrogenVehicals.json", "w", encoding="utf-8") as f:
+               json.dump(state, f, indent=4, ensure_ascii=False)
+    print("Planner Agent Ended")
+
     return state
  
    
-test_state = {
-        "messages": [
-            {"role": "user", "content": "What is the market size of Electric Vehical in USA?"}
-        ],
-        "tasks": []
-    }
-result=taskPlannerNode(test_state)
-print(result)
+# test_state = {
+#         "messages": [
+#             {"role": "user", "content": "What is the AI in healthcare market size in the United States from 2020–2025?"}
+#         ],
+#         "tasks": []
+#     }
+# result=taskPlannerNode(test_state)
+# print(result)
